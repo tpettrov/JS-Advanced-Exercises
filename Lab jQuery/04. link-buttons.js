6bf116e36b8fa3d0f36f178287e9f1fad22e1311
tpettrov <tpettrov@gmail.com>
2016-10-22 21:57:02 +0300

    04.

---
+++ Lab jQuery/04. link-buttons.js
@@ -0,0 +1,17 @@
+function attachEvents() {
+
+    $('a.button').on('click', buttonClicked);
+
+
+    function buttonClicked(){
+
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
+
+
+    }
+
+
+
+}
+
---
+++ Lab jQuery/04..js
@@ -0,0 +1,16 @@
+function main(input) {
+
+for (let i=0; i<=100; i++) {
+
+
+    console.log(i%57);
+}
+
+}
+
+main([
+    'field=value1&field=value2&field=value3',
+    'http://example.com/over/there?name=ferret',
+
+
+]);
