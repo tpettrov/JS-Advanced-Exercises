1fe486e13cefbe609a65f14be251f7782ce3ffa4
tpettrov <tpettrov@gmail.com>
2016-10-25 23:43:48 +0300

    solved

---
+++ jQuerry & DOM/02. fibonacci.js
@@ -0,0 +1,30 @@
+function fibonaci(n){
+
+    let calcPrintFib = (function(){
+
+        let num0 = 0;
+        let num1 = 1;
+
+        return ()=> {
+            let oldnum0 = num0;
+            let oldnum1 = num1;
+            num0 = oldnum1;
+            num1 = oldnum0 + oldnum1;
+            return num0;
+        }
+
+    })();
+
+    let arr = [];
+
+    for (let i=0; i<n; i++) {
+
+        arr.push(calcPrintFib());
+
+
+    }
+
+return arr;
+}
+
+fibonaci(5);
