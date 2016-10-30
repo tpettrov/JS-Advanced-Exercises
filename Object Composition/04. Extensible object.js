b096587aae04d53b579010af8e3d4deefb72cb55
tpettrov <tpettrov@gmail.com>
2016-10-30 09:59:03 +0200

    solution

--- Object Composition/04. Extensible object.js
+++ Object Composition/04. Extensible object.js
@@ -1,13 +1,33 @@
 function solve(){
 
-    let myObj = {
+    let myObj = {};
+    myObj.extend = function(template){
 
-        __proto__: {},
-        extend: function (){}
-    };
+        for (let prop in template) {
+
+            if (typeof(template[prop]) == "function") {
+
+                Object.getPrototypeOf(myObj)[prop] = template[prop];
+            }
+
+            myObj[prop] = template[prop];
+
+        }
+
+    }
 
 
-
+    return myObj;
 
 
 };
+
+
+let template =  {
+    extensionMethod: function () {console.log('function from template')},
+    extensionProperty: 'someString'
+};
+
+let returnedObj = solve();
+returnedObj.extend(template);
+console.log(returnedObj);
